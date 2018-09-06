854b0b017bd581656bd8a45f621ded33af2559cd
mahy <992635910@qq.com>
2018-09-06 18:41:44 +0800

    init

--- process_data.py
+++ process_data.py
@@ -2,7 +2,7 @@ import cv2
 import glob
 
 
-def keyframe_extraction(video):
+def keyframe_extraction(video,frame_path):
     cap = cv2.VideoCapture(video)
 
     fps = cap.get(cv2.CAP_PROP_FPS)  # 获取帧速
@@ -18,10 +18,10 @@ def keyframe_extraction(video):
     success, frame = cap.read()
     i = 0
     while success:
-        print('%s%s' % (video, i))
+        print('%s,%s' % (video, i))
         success, arr = cv2.imencode('.jpg', frame)
         a = arr.tostring()
-        fp = open(path + 'frame/' + video.split('.')[0] + str(i) + '.jpg', 'wb')
+        fp = open(frame_path + video.split('.')[0].split('/')[4] + '_' + str(i) + '.jpg', 'wb')
         fp.write(a)
         fp.close()
         i = i + 1
@@ -34,5 +34,6 @@ def get_video(path):
 
 if __name__ == '__main__':
     path = 'D:/spwd/VQADatasetA_20180815/'
+    frame_path = 'D:/spwd/VQADatasetA_20180815/frame/'
     for i in get_video(path):
-        keyframe_extraction(i)
+        keyframe_extraction(video=i.replace('\\', '/'), frame_path=frame_path)
